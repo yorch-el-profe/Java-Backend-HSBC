2975118f5bc0748169c7b90436e753aa4f57ffc4
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-12-14 21:59:00 -0600

    Agregando configuracion de logger

--- 07-logger/02-configuracion/src/main/java/org/bedu/logger/Runner.java
+++ 07-logger/02-configuracion/src/main/java/org/bedu/logger/Runner.java
@@ -10,12 +10,6 @@ public class Runner implements CommandLineRunner {
 
   private static Logger logger = LoggerFactory.getLogger(Runner.class);
 
-  /*
-   * DEBUG: Mensajes no tan importantes
-   * INFO: Cosas importantes que pasan en el código
-   * WARN: Problemas recuperables
-   * ERROR: Problemas irrecuperables
-   */
   @Override
   public void run(String[] args) {
     String name = "Demo de Loggers";
@@ -26,16 +20,3 @@ public class Runner implements CommandLineRunner {
     logger.debug("App: {}", name);
   }
 }
-
-/**
- * Ejercicio:
- * Crear una clase llamada Primos que valide si un numero N es primo.
- * Un número primo es aquel que es divisible por la unidad y si mismo.
- * 2,3,5,7,11,13,...
- * 
- * Utilizar un logger INFO si el número efectivamente es primo
- * Utilizar un logger ERROR si el número es negativo
- * Utilizar un logger WARN si el número no es primo
- * 
- * Crear un método run (CommandLineRunner) para probar el código
- */
