097ddcd24a2d0b929e81e5eb01c3e56d151bda49
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-12-17 11:53:33 -0600

    Agregando ultimos metodos

--- circulo-estudio/api/src/main/java/org/bedu/api/controller/CourseController.java
+++ circulo-estudio/api/src/main/java/org/bedu/api/controller/CourseController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.bedu.api.dto.CourseDTO;
 import org.bedu.api.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,7 +33,20 @@ public class CourseController {
   }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public CourseDTO save(@RequestBody CourseDTO data) {
     return service.save(data);
+  }
+
+  @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void update(@PathVariable("id") long id, @RequestBody CourseDTO data) {
+    service.update(id, data);
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void delete(@PathVariable("id") long id) {
+    service.delete(id);
   }
 }
