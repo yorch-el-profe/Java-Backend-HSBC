74ac777d20e932ef01bf1a372188ec86d09fcca5
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-12-16 20:29:39 -0600

    Agregando configuracion para el consumidor de RabbitMQ

---
+++ 06-jms/consumer/src/main/java/org/bedu/jms/JmsApplication.java
@@ -0,0 +1,13 @@
+package org.bedu.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsApplication.class, args);
+	}
+
+}
